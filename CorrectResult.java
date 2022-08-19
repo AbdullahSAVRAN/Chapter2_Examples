9da2d95413ca1c9506dbb5eeff7a0fd82e292b78
AbdullahSAVRAN <111440438+AbdullahSAVRAN@users.noreply.github.com>
2022-08-19 11:10:47 +0300

    Add files via upload

---
+++ CorrectResult.java
@@ -0,0 +1,14 @@
+package Workshop;
+
+public class CorrectResult {
+
+	public static void main(String[] args) {
+		System.out.println(2 * (5 / 2 + 5 / 2));
+		System.out.println(2 * 5 / 2 + 2 * 5 / 2);
+		System.out.println(2 * (5 / 2));
+		System.out.println(2 * 5 / 2);
+		System.out.println(2 * (5 / 2.0 + 5 / 2.0));
+
+	}
+
+}
---
+++ Increment.java
@@ -0,0 +1,25 @@
+package Workshop;
+
+public class Increment {
+
+	public static void main(String[] args) {
+		int a = 6;
+		System.out.println(a);
+		int b = a++;
+		System.out.println(b);
+		int c = 6;
+		System.out.println(c);
+		int d = ++c;
+		System.out.println(d);
+		int x = 6;
+		int y = x++;
+		System.out.println(x);
+		System.out.println(y);
+		int k = 6;
+		int l = ++k;
+		System.out.println(k);
+		System.out.println(l);
+
+	}
+
+}
